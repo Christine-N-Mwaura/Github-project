4b9ddd3ee99504905a3338fbd4d4b3ff3cb52aeb
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-08-23 17:46:07 +0300

    add an event emitter in the function definition of search function

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-
+//  username: string                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+   @Output() emitUserName = new EventEmitter<string>()
+   search(username){
+     this.emitUserName.emit(username)
+   }
   constructor() { }
 
   ngOnInit() {
