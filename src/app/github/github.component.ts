ce34338ae4b99b899ccbb13d913a0efd2c237e86
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-08-23 17:41:12 +0300

    add a function to be executed when the emitted event is received

--- src/app/github/github.component.ts
+++ src/app/github/github.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubComponent implements OnInit {
 
+  searchUserName(username){
+  
+  }
   constructor() { }
 
   ngOnInit() {
