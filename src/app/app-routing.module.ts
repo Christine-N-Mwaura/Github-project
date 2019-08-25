28dff96433ec788523deb3d8490128320c0be92e
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-08-25 08:26:32 +0300

    import components to be routed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {GithubComponent} from './github/github.component';
+import {ReposComponent } from './repos/repos.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+   {"path":"home","component":GithubComponent},
+   {"path":"repos","component":ReposComponent},
+   {"pathMatch":"full", "redirectTo":"home","path":""},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+ 
