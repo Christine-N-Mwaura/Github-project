4d46ac37b07016b6f0ddbd552f82e1b645a9c71d
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-08-25 08:40:56 +0300

    use a service to store user data from the api

--- src/app/github/github.component.ts
+++ src/app/github/github.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpServiceService } from '../service/http-service.service';
+import {Users} from '../users'
 @Component({
   selector: 'app-github',
   templateUrl: './github.component.html',
   styleUrls: ['./github.component.css']
 })
 export class GithubComponent implements OnInit {
-
+  users:Users[];
   searchUserName(username){
-  
+   this.httpServiceService.findUser(username).then(
+     ()=>{
+       this.users = this.httpServiceService.users;
+       console.log(this.users);
+     },
+     (error)=>{
+       console.log(error)
+     }
+   )
   }
-  constructor() { }
+  constructor(public httpServiceService:HttpServiceService) { }
 
   ngOnInit() {
   }
